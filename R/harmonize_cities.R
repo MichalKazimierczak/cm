bbbf32735fef4da65542b426828be2e637f8b751
MichalKazimierczak <kazimierczakk@yahoo.co.uk>
2024-10-03 17:25:43 +0200

    Update harmonize_cities.R

--- R/harmonize_cities.R
+++ R/harmonize_cities.R
@@ -1,25 +1,23 @@
 #' @import data.table
 #' @import stringr
 
-#' @title returns a dataset with ordered and ranked matches
+#' @title returns a dataset with harmonized names of the cities
 #'
-#' @description This function is being used after the disambiguation of matches to rank possible candidates for the match
-#' @param r cleaned dataset to be ranked
+#' @description This function allows for proper comparison of the cities' names if they come from different sources
+#' @param r dataset containing names of the cities
 #'
-#' @param prefer_root logical variable indicating whether the ORBIS observations with the bvd_id number without hyphen should be preferred
-#' @param bvd_is_root weight assigned to the criterion of bvd_id_number not to include hyphen
-#' @param name_sim weight assigned to the similarity of original names
-#' @param lf_sim weight assigned to the similarity of legal forms
-#' @param reg_sim weight assigned to the similarity of seat regions as shown in respective datasets
-#' @param city_sim weight assigned to the similarity of cities of seat as shown in respective datasets
-#' @param street_sim weight assigned to the similarity of cities of seat as shown in respective datasets
+#' @param city_a indicates a name of the column storing a name of city A
+#' @param city_b indicates a name of the column storing a name of city B
+#' @param cc indicates a code of a country where the cities A and B are located
+#' @param new_col if TRUE creates new columns with normalized version of the cities A and B names
+#'along with the original version of the name
 #' @returns an original dataframe with ordered and ranked matched dataframe
 #' @examples
 #' @export
 
 
-harmonize_cities<-function(r,city_a,city_b,cc){
-  data(cities)
+harmonize_cities<-function(r,city_a,city_b,cc,new_col=T){
+  #data(cities)
   citiesc<-cities[cities$Country.Code==cc,]
   r[,city_a]<-stringr::str_replace_all(r[,city_a], "[^a-zA-Zα-ωΑ-Ωa-яA-Я]"," ")
   r[,city_a]<-stringr::str_replace_all(r[,city_a],"\\s+"," ")
@@ -27,18 +25,33 @@ harmonize_cities<-function(r,city_a,city_b,cc){
   r[,city_b]<-stringr::str_replace_all(r[,city_b],"\\s+"," ")
   r[,city_a]<-stringi::stri_trans_general(r[,city_a], "latin-ascii; upper")
   r[,city_b]<-stringi::stri_trans_general(r[,city_b], "latin-ascii; upper")
-  r[,city_a]<-str_trim(r[,city_a])
-  r[,city_b]<-str_trim(r[,city_b])
+  r[,city_a]<-stringr::str_trim(r[,city_a])
+  r[,city_b]<-stringr::str_trim(r[,city_b])
   r$city_norm_a<-""
   r$city_norm_b<-""
-  for (i in 1:nrow(citiesc)){
-    cit<-citiesc[i,"alt_names"]
-    r$city_norm_a<-ifelse(r$city_norm_a==""&stringr::str_detect(r[,city_a],cit),as.character(citiesc[i,"ASCII.Name"]),r$city_norm_a)
-    r$city_norm_b<-ifelse(r$city_norm_b==""&stringr::str_detect(r[,city_b],cit),as.character(citiesc[i,"ASCII.Name"]),r$city_norm_b)
-    print(i)
+  # for (i in 1:nrow(citiesc)){
+  #   cit<-as.character(citiesc[i,"Alternate.Names"])
+  #   r$city_norm_a<-ifelse(r$city_norm_a==""&stringr::str_detect(r[,city_a],cit),as.character(citiesc[i,"ASCII.Name"]),r$city_norm_a)
+  #   r$city_norm_b<-ifelse(r$city_norm_b==""&stringr::str_detect(r[,city_b],cit),as.character(citiesc[i,"ASCII.Name"]),r$city_norm_b)
+  # }
+
+  r$city_norm_a<-ifelse(r$city_norm_a=="",r[,city_a],r$city_norm_a)
+  r$city_norm_b<-ifelse(r$city_norm_b=="",r[,city_b],r$city_norm_b)
+
+  if(new_col==T){
+    cla<-paste0(colnames(r)[str_detect(city_a,colnames(r))],"_harmonized")
+    colnames(r)[str_detect(colnames(r),"city_norm_a")]<-cla
+    clb<-paste0(colnames(r)[str_detect(city_b,colnames(r))],"_harmonized")
+    colnames(r)[str_detect(colnames(r),"city_norm_b")]<-clb
+
+  } else{
+    r[,city_a]<-r$city_norm_a
+    r[,city_b]<-r$city_norm_b
+    r$city_norm_a<-NULL
+    r$city_norm_b<-NULL
+
   }
 
 
   return(r)
 }
-
